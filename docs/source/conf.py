d8315d7d0e3805661d784b30d4d070773a231ab3
unknown <awdcgy@hanyang.ac.kr>
2023-02-09 14:41:33 +0900

    modify nsp ceil

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
 templates_path = ['_templates']
 exclude_patterns = []
--- docs/source/post_training/train.rst
+++ docs/source/post_training/train.rst
@@ -3,5 +3,4 @@ post_training train code
 --------------------------------------------
 train code에 대한 설명입니다.
 
-
 print("hello world!")
