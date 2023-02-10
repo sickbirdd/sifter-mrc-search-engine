767699c1b2152f5b52fd9c9d1a63f899a9655019
unknown <awdcgy@hanyang.ac.kr>
2023-02-10 14:22:11 +0900

    modify fine-tuning train class

--- modules/main.py
+++ modules/main.py
@@ -1,12 +1,11 @@
 from lm_post_training.train import train
-from mrc_fine_tuning.Train import fine_tuning_trainer, fine_tuning_evaluator
+from mrc_fine_tuning.train_futher_release import FineTuning
 import sys
 
 if __name__ == '__main__':
     print("Main Module Execute")
 
     # 인자 체크
-    print(sys.argv)
     if len(sys.argv) < 2:
         print("인자가 부족합니다.")
         print("다음과 같은 해결책이 있습니다.")
@@ -21,10 +20,10 @@ if __name__ == '__main__':
         train()
     elif sys.argv[1] == 'fine-tuning':
         print("FINE_TUNING")
-        fine_tuning_trainer()
+        FineTuning().fine_tuning_trainer('train')
     elif sys.argv[1] == 'eval':
         print("EVAL MODE")
-        fine_tuning_evaluator()
+        FineTuning().fine_tuning_trainer('else')
     else:
         print("올바른 명령어가 아닙니다.")
         print("다음과 같은 해결책이 있습니다.")
