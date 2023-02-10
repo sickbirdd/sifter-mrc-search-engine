fada6448c55df1074d073d2a196d09bd79e6eac8
ByeongIk Choi <bichoi0715@gmail.com>
2023-02-10 13:14:12 +0900

    fixed make html error
    Makefile 문제 해결

--- modules/loader.py
+++ modules/loader.py
@@ -3,11 +3,17 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 import yaml
-with open('config_pt.yaml') as f:
-    conf_pt = yaml.safe_load(f)
     
-with open('config_ft.yaml') as f:
-    conf_ft = yaml.safe_load(f)
+if __name__ == '__main__':
+    with open('config_pt.yaml') as f:
+        conf_pt = yaml.safe_load(f)
     
-with open('config_log.yaml') as f:
-    conf_log = yaml.safe_load(f)
+    with open('config_ft.yaml') as f:
+        conf_ft = yaml.safe_load(f)
+        
+    with open('config_log.yaml') as f:
+        conf_log = yaml.safe_load(f)
+else:
+    conf_pt = {}
+    conf_ft = {}
+    conf_log = {}
