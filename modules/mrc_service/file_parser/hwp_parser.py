3b9860c528cc43c6f6c3e45ef9569cc1656365d3
ByeongIk Choi <bichoi0715@gmail.com>
2023-02-28 13:55:47 +0900

    remove hwp

--- modules/mrc_service/file_parser/hwp_parser.py
+++ modules/mrc_service/file_parser/hwp_parser.py
@@ -8,8 +8,8 @@ class HwpParser(Parser):
         ole= olefile.OleFileIO(buffer)
         return ole.openstream('PrvText').read().decode('utf-16')
     
-    def parse(self, buffer, length, cond_split="\n\n\n") -> list:
+    def parse(self, buffer, length, cond_split="\n") -> list:
         content = self.parser_buffer(buffer)
-
+        print(content)
         paragragh_list = [paragragh for paragragh in content.split(cond_split) if len(paragragh) > length]
         return paragragh_list
--- modules/mrc_service/file_parser/pdf_parser.py
+++ modules/mrc_service/file_parser/pdf_parser.py
@@ -8,7 +8,6 @@ class PDFParser(Parser):
     
     def parse(self, buffer, length, cond_split="\n\n\n") -> list:
         data = self.parser_buffer(buffer)
-
         content = data['content']
         paragragh_list = [paragragh for paragragh in content.split(cond_split) if len(paragragh) > length]
         return paragragh_list
